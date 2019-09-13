169928e5c22e459d99790aaa61b93449b16b556a
meowpeow <meowpeow3@gmail.com>
2019-09-13 20:06:51 +0300

    Before adding generics to interface
    Before adding generics to interface

--- src/gameOOP/events/Spawn.java
+++ src/gameOOP/events/Spawn.java
@@ -3,7 +3,7 @@ package gameOOP.events;
 import gameOOP.combatants.Character;
 import gameOOP.combatants.MainHeroPC;
 
-interface Spawn<T> {
+interface Spawn {
 
     MainHeroPC create(MainHeroPC mainhero);
 
